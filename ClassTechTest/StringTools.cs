2157582eabae52a61269972572f2a5c1f1c21526
Tien Tran <tmytien@tma.com.vn>
2024-11-15 14:24:22 +0700

    implement FindFirstDuplicatedCharacter to find first duplicate char

--- ClassTechTest/StringTools.cs
+++ ClassTechTest/StringTools.cs
@@ -4,8 +4,32 @@
     {
         #region String Tool Problems
 
+        // <summary>
+        // Find and return first duplicated char
+        // </summary>
+        // <param name="input"></param>
+        // <returns>first duplicated char</returns>
         public string FindFirstDuplicatedCharacter(string input)
         {
+            if (string.IsNullOrWhiteSpace(input))
+                return null;
+
+            var seenChars = new string[input.Length];
+            int index;
+            foreach(var c in input)
+            {
+                var cString = c.ToString().ToUpper();
+                for (int i = 0; i < index; i++)
+                {
+                    // Check if string is existed in seenChars array
+                    if (seenChars[i] == cString)
+                        return cString;
+                }
+                // If not, add string into array
+                seenChars[index] = cString;
+                index ++;
+            }
+
             return null;
         }
 
