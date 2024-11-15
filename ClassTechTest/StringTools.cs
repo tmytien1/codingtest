d787846369c6df3310fa15a1b3220fa8346b8b39
Tien Tran <tmytien@tma.com.vn>
2024-11-15 14:45:58 +0700

    implement IsPalindrome to check if string is palindrome

--- ClassTechTest/StringTools.cs
+++ ClassTechTest/StringTools.cs
@@ -1,5 +1,7 @@
 ﻿namespace ClassTechTest
 {
+    using System;
+
     public class StringTools
     {
         #region String Tool Problems
@@ -15,7 +17,7 @@
                 return null;
 
             var seenChars = new string[input.Length];
-            int index;
+            int index = 0;
             foreach(var c in input)
             {
                 var cString = c.ToString().ToUpper();
@@ -37,8 +39,30 @@
 
         #region Palindrome Problems
 
+        // <summary>
+        // Check if string is palindrome
+        // </summary>
+        // <param name="input"></param>
+        // <returns>true if string is palindrome otherwise return false </returns>
         public bool IsPalindrome(string input)
         {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            var array = input.ToCharArray();
+            int leftIndex = 0;
+            int rightIndex = array.Length - 1;
+
+            // While left index < right index, check if left char is the same with right char
+            while (leftIndex < rightIndex)
+            {
+                if (char.ToLower(array[leftIndex]) != char.ToLower(array[rightIndex]))
+                    return false;
+
+                leftIndex ++;
+                rightIndex --;
+            }
+
             return true;
         }
         
